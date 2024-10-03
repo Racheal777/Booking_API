794eaec13bd10a05bc7c98bd6ab0effc53f1ca00
rachealkuranchie <kuranchieracheal@gmail.com>
2024-10-03 15:20:51 +0000

    added cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import { bookingRouter } from './routes/all-routes.js'
 import mongoose from 'mongoose'
 import 'dotenv/config'
+import cors from 'cors'
 
 const app = express()
 
 const PORT = 7079
 
+app.use(cors())
 app.use(express.json())
 app.use(bookingRouter)
 
